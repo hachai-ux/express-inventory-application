7efe314e2864f9e196d856ff6514e10d4ca50460
Hac Hai Pham <hpham.ux@gmail.com>
2022-01-31 12:01:03 +0100

    Add index to controllers

--- routes/menu.js
+++ routes/menu.js
@@ -5,8 +5,7 @@ var category_controller = require('../controllers/categoryController');
 var item_controller = require('../controllers/itemController');
 
 // Home page route.
-router.get('/', function (req, res) {
-  res.send('Category home page');
+router.get('/', category_controller.index);
 })
 
 
