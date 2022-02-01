4ac247920cd2b4ecd9030cad70c855c2fd7f3d84
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-01 15:21:55 +0100

    Create post form for categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,8 @@ var Category = require('../models/category');
 var Item = require('../models/item');
 var async = require('async');
 
+const { body,validationResult } = require("express-validator");
+
 exports.index = function(req, res, next) {
     //counter
     /*
@@ -44,15 +46,62 @@ exports.category_detail = function(req, res, next) {
 };
 
 
-// Display Category create form on GET.
-exports.category_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+// Display category create form on GET.
+exports.category_create_get = function(req, res, next) {
+    res.render('category_form', { title: 'Create Category' });
 };
 
-// Handle Category  create on POST.
-exports.category_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+// Handle Category create on POST.
+exports.category_create_post =  [
+
+  // Validate and sanitize the name field.
+    body('name', 'Category name required').trim().isLength({ min: 1 }).escape(),
+    body('description', 'Category description required').trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var category = new Category(
+        {
+            name: req.body.name,
+            description: req.body.description
+        }
+    );
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('description_form', { title: 'Create Category', category: category, errors: errors.array()});
+      return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Category.findOne({ 'name': req.body.name })
+        .exec( function(err, found_category) {
+           if (err) { return next(err); }
+
+           if (found_category) {
+             // Category exists, redirect to its detail page.
+             res.redirect(found_category.url);
+           }
+           else {
+
+             category.save(function (err) {
+               if (err) { return next(err); }
+               // Item saved. Redirect to item detail page.
+               res.redirect(category.url);
+             });
+
+           }
+
+         });
+    }
+  }
+];
 
 // Display Category  delete form on GET.
 exports.category_delete_get = function(req, res) {
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 var Item = require('../models/item');
+const { body,validationResult } = require("express-validator");
 
 // Display detail page for a specific Item.
 exports.item_detail = function(req, res, next) {
@@ -12,14 +13,62 @@ exports.item_detail = function(req, res, next) {
 };
 
 // Display Item create form on GET.
-exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+exports.item_create_get = function(req, res, next) {
+    res.render('item_form', { title: 'Create Item' });
 };
 
 // Handle Item create on POST.
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.item_create_post =  [
+
+  // Validate and sanitize the name field.
+    body('name', 'Item name required').trim().isLength({ min: 1 }).escape(),
+    body('description', 'Item description required').trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var item = new Item(
+        {
+            name: req.body.name,
+            description: req.body.description
+        }
+    );
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('item_form', { title: 'Create Item', item: item, errors: errors.array()});
+      return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Item.findOne({ 'name': req.body.name })
+        .exec( function(err, found_item) {
+           if (err) { return next(err); }
+
+           if (found_item) {
+             // Genre exists, redirect to its detail page.
+             res.redirect(found_item.url);
+           }
+           else {
+
+             item.save(function (err) {
+               if (err) { return next(err); }
+               // Item saved. Redirect to item detail page.
+               res.redirect(item.url);
+             });
+
+           }
+
+         });
+    }
+  }
+];
+
 
 // Display Itemdelete form on GET.
 exports.item_delete_get = function(req, res) {
